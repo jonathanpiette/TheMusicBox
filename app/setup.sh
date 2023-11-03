3010dacdd56470a38778c53b8622532215c9b235
Jonathan Piette <dev@reciplist.com>
2023-11-03 14:07:55 +0100

    update setup.sh with Pisugar installation

--- app/setup.sh
+++ app/setup.sh
@@ -32,6 +32,10 @@ echo "Configuring FTP access..."
 echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf
 sudo systemctl restart vsftpd
 
+# Installing Pisugar Battery Management Software
+echo "Installing Pisugar Battery Management Software..."
+curl http://cdn.pisugar.com/release/pisugar-power-manager.sh | sudo bash
+
 # Additional setup could be included here, such as adding the user to the SPI and GPIO groups
 echo "Adding user to 'spi' and 'gpio' groups..."
 sudo usermod -a -G spi,gpio,i2c $(whoami)
@@ -48,7 +52,6 @@ The music box is a special project for you Arthur. I made
 it with love just for you so you can discover
 great music!
 -----------------------------------------------"
-
 
 echo "Installation completed successfully."
 echo "Please reboot the Raspberry Pi for the changes to take effect."
